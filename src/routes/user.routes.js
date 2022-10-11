378518e266b4a6a70f490386a273b94dcf2788f7
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-11 11:36:49 -0500

    tutorAndPlayer

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getPlayers } from "../controllers/user.controller.js";
+import { getPlayers, getTutors, insertPlayer, insertTutor } from "../controllers/user.controller.js";
 
 
 const router = Router();
 
-router.get('/players', getPlayers);
+router.get('/get/players', getPlayers);
+router.get('/get/tutors', getTutors);
+
+router.post('/add/player', insertPlayer)
+router.post('/add/tutor', insertTutor)
 
 
 export default router;
