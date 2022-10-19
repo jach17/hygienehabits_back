320b8f378e6a70d8333986fc0cc0e89f3f4917a4
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-18 20:25:03 -0500

    update

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT
-export const DB_HOST = process.env.DB_HOST
-export const DB_PORT = process.env.DB_PORT
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1"
+export const DB_PORT = process.env.DB_PORT||3306
 export const DB_USER = process.env.DB_USER
 export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_DATABASE = process.env.DB_DATABASE || hygienehabits_db
