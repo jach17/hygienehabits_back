c0a5c2c2ea80a680c6b1a583117e3d264d0d8e48
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-24 17:42:47 -0500

    Add app to refactor code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import playerRoutes from './routes/player.routes.js'
+
+
+const api = '/api/hygienehabits';
+const app = express();
+app.use(express.json());
+/*****
+ * Test api url and db connection
+ */
+app.use(`${api}/test/dbconnection`,playerRoutes);
+
+
+app.use((req, res, next)=>{
+    res.status(404).json({
+        message:"La ruta especificada no coincide con ninguna... para mayor información pongase en contacto con el cuchau c:"
+    });
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-import express from 'express';
-import playerRoutes from './routes/player.routes.js'
+import app from './app.js'
 import {PORT} from './config.js'
 
-const api = '/api/hygienehabits';
-const app = express();
-app.use(express.json());
-/*****
- * Test api url and db connection
- */
-app.use(`${api}/test/dbconnection`,playerRoutes);
 
-
-app.use((req, res, next)=>{
-    res.status(404).json({
-        message:"La ruta especificada no coincide con ninguna... para mayor información pongase en contacto con el cuchau c:"
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server started on port`, PORT);
-});
+app.listen(PORT);
+console.log("Server running on port ", PORT);
