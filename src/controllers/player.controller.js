d21e71a791e73462889f5adbfbb1801ecf62a037
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-25 22:05:59 -0500

    JSON responses working on test routes

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -1,13 +1,10 @@
 import { pool } from "../db.js";
-/*****
+/**
  * Rutas de prueba de conexion y manipulacion con las bases de datos
- * 
- * 
  */
 
 export const testNoDBRoute =(req, res)=>{
     try{
-        throw "Error de prueba bro xd";
         res.status(200).json(
             {
                 "result":"1",
@@ -23,7 +20,7 @@ export const testNoDBRoute =(req, res)=>{
             }
         );
     }catch(e){
-        res.status(500).json(
+        return res.status(500).json(
             {
                 "result":"0",
                 "message":
@@ -44,7 +41,6 @@ export const testNoDBRoute =(req, res)=>{
 export const testGetRoute = async(req, res)=>{
         try{
             const [result] = await pool.query('SELECT * FROM tb_player');
-            
             res.status(200).json(
                 {
                     "result":"1",
@@ -55,37 +51,94 @@ export const testGetRoute = async(req, res)=>{
                     "code":"200"
                 }
             );
-            
-
-            
-        }catch(error){
-            return res.json({
-                message:'Algo salió mal xd',
-                er:error
-
-            });
-
+        }catch(e){
+            return res.status(500).json(
+                {
+                    "result":"0",
+                    "message":
+                    {
+                        "response":[
+                            {
+                                "Error": e,
+                            },
+                        ]
+                    },
+                    "code":"500"
+                }
+            );
         }
     }
 
 export const testPostRoute = async(req, res)=>{
     try{
         const {namePlayer, passwordPlayer, agePlayer, idTutorOwner, authTokenTutor} = req.body
-    const [row] = await pool.query('INSERT INTO tb_player (namePlayer, passwordPlayer, agePlayer, idTutorOwner, authTokenTutor) VALUES (?,?,?,?,?)', [namePlayer, passwordPlayer, agePlayer, idTutorOwner, authTokenTutor]);
-    console.log("db connection and routes con post");
-    res.json({'Inserted id':row.insertId});
-    }catch(error){
-return res.json({
-                message:'Algo salió mal xd',
-                er:error
-
-            });
-            
+        const query='INSERT INTO tb_player (namePlayer, passwordPlayer, agePlayer, idTutorOwner, authTokenTutor) VALUES (?,?,?,?,?)';
+        const [row] = await pool.query(query, [namePlayer, passwordPlayer, agePlayer, idTutorOwner, authTokenTutor]);
+        res.status(200).json(
+            {
+                "result":"1",
+                "message":
+                {
+                    "response":[
+                        {
+                            "insertedId":row.insertId
+                        }
+                    ]
+                },
+                "code":"200"
+            }
+        );
+    }catch(e){
+        return res.status(500).json(
+            {
+                "result":"0",
+                "message":
+                {
+                    "response":[
+                        {
+                            "Error": e,
+                        },
+                    ]
+                },
+                "code":"500"
+            }
+        );  
     }
 }
 
 export const testGetRouteParams = async(req, res)=>{
-    const idToFind = req.params.id
-    const [result] = await pool.query('SELECT * FROM tb_player WHERE idPlayer=?', idToFind);
-    res.send(result[0]);
+    try{
+        const idToFind = req.params.id
+        const query='SELECT * FROM tb_player WHERE idPlayer=?';
+        let [result] = await pool.query(query, idToFind);
+        if(result.length==0){
+            result= [{"Error":"El id solicitado no se encuentra registrado"}];
+        }
+        res.status(200).json(
+            {
+                "result":"1",
+                "message":
+                {
+                    "response":result
+                },
+                "code":"200"
+            }
+        );
+    }catch(e){
+        return res.status(500).json(
+            {
+                "result":"0",
+                "message":
+                {
+                    "response":[
+                        {
+                            "Error": e,
+                        },
+                    ]
+                },
+                "code":"500"
+            }
+        );
+    }
+
 }
