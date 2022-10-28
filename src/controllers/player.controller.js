4f9c773a1b3db30e43c9a7d258978c66e1c4a867
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-27 23:17:52 -0500

    Get all players works

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -100,13 +100,14 @@ export const getPlayers = async(req, res)=>{
         const query = 'SELECT * FROM tb_player';
         let [result] = await pool.query(query);
         if(isJSONempty(result)){
+            console.log("Json empty")
             result = [{"Error":"No hay jugadores registrados"}];
         }
         res.status(200).json(
             jsonResponse(
-                RESULT_CODE_ERROR,
-                e,
-                STATUS_CODE_ERROR
+                RESULT_CODE_SUCCESS,
+                result,
+                STATUS_CODE_SUCCESS
             )
         );
     }catch(e){
