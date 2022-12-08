7dc643c48834f05d76782e6b451a748c4e21154a
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-12-08 15:59:54 -0600

    Estandar on report feedback response

--- src/controllers/report.controller.js
+++ src/controllers/report.controller.js
@@ -76,11 +76,11 @@ export const addTutorFeedback = async (req, res) => {
     const query = `UPDATE tb_report SET tutorFeedback=?  WHERE idReport=${idToUpdate}`;
     const [row] = await pool.query(query, [tutorFeedback]);
     if (row.affectedRows != 0) {
-      response = [{ insertedId: "true" }];
+      response = [{ insertedId: idToUpdate }];
     } else {
       response = [
         {
-          insertedId: "false",
+          insertedId: 0,
         },
       ];
     }
