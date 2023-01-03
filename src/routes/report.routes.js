468489219808d07177c0acb5cea25f0ce3888676
Jonathan Dev <jonathan.aldana170500@gmail.com>
2023-01-02 19:18:36 -0600

    Restar service

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -17,5 +17,6 @@ reportRouter.get("/list/reports/player/:id", getReportsByPlayerId);
 reportRouter.post("/add/feedback/report/:id", addTutorFeedback);
 reportRouter.get("/list/fullreports/player/:id", getFullReportByPlayer);
 reportRouter.get("/list/fullreports/sessions/:id", getFullReportBySessionId);
+/*Restart service*/
 
 export default reportRouter;
